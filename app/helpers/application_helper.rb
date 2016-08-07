cca1f8206e3fe3581b6c0c042590a3880876504c
Raphael Monteiro <rmonteiro89@hotmail.com>
2016-08-07 19:20:06 +0200

    Fix view

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,6 +2,7 @@ module ApplicationHelper
 
   def flash_messages(opts = {})
     flash.each do |msg_type, message|
+      message = message.html_safe
       concat(content_tag(:div, message, class: "alert #{bootstrap_class_for(msg_type)} fade in text-center") do
               concat content_tag(:button, 'x', class: "close", data: { dismiss: 'alert' })
               concat message
--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,7 +1,8 @@
 module ExpensesHelper
-
   def expense_created_message(expense)
-    link_to_edit = view_context.link_to 'edit', '#', 'data-toggle'=>'modal', 'data-target'=> "#editExpenseModal_#{expense.id}"
-    "Expense created successfully!</br> #{expense.description}, #{expense.amount.format}, #{I18n.l(expense.date)}, #{expense.category_name} (#{link_to_edit})"
+    link_to_edit = view_context.link_to 'edit', '#', 'data-toggle' => 'modal', 'data-target' => "#editExpenseModal_#{expense.id}"
+    "Expense created successfully!</br>
+    #{expense.description}, #{expense.amount.format}, #{I18n.l(expense.date)},
+    #{expense.category_name}"
   end
 end
