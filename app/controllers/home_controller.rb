d2e9469c0477ea96010bf920fa893b8b8ceb5e9e
Raphael Monteiro <rmonteiro89@hotmail.com>
2016-08-07 16:05:23 +0200

    Redirect to trips from home if user signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,9 @@
 class HomeController < ApplicationController
   def index
-    @user = User.new
+    if current_user.present?
+      redirect_to trips_path
+    else
+      @user = User.new
+    end
   end
 end
